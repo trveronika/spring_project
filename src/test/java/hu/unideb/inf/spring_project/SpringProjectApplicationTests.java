eb4204cd2d2b7200ec7a5c604776c477e97ac062
tári_veronika <tarivera1998@gmail.com>
2023-11-20 12:57:47 +0100

    Initial commit

---
+++ src/test/java/hu/unideb/inf/spring_project/SpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
